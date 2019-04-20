4a0ef5001a1dc51d2001e047cfafb008937f1435
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-04-20 13:02:10 -0300

    display info about time machines

--- src/GameCardTypes/TimeMachine.js
+++ src/GameCardTypes/TimeMachine.js
@@ -2,8 +2,21 @@ import React from 'react';
 import './TimeMachine.scss';
 import classNames from 'classnames';
 import {TIME_MACHINE} from "./GameCardTypes";
+import {LANGUAGES, withCurrentLanguage} from "../Languages";
 
-const TimeMachine = ({ className, card }) => {
+const REQUIRES_LABEL = {
+  [`${LANGUAGES.ENGLISH}`]: 'requires',
+  [`${LANGUAGES.SPANISH}`]: 'necesita'
+};
+const isFullyEquipped = (card) => !card.requires;
+
+const renderRequirements = (cardRequirement, currentLanguage) => <div className={'title-description'}>
+    {REQUIRES_LABEL[currentLanguage]}: {cardRequirement}
+</div>;
+
+const renderSubtitle = (subtitle) => <div className={'title-description'}>{subtitle}</div>;
+
+const TimeMachineCard = ({ className, card, currentLanguage }) => {
     const classes = classNames(className, 'bttf-card', 'time-machine');
     return (
         <div className={classes}>
@@ -12,6 +25,7 @@ const TimeMachine = ({ className, card }) => {
             </div>
             <div className={'bttf-card-body'}>
                 {card.title}
+                {isFullyEquipped(card) ? renderSubtitle(card.subtitle) : renderRequirements(card.requires, currentLanguage) }
             </div>
             <div className={'bttf-card-footer'}>
                 {card.description}
@@ -20,6 +34,7 @@ const TimeMachine = ({ className, card }) => {
     );
 };
 
+const TimeMachine = withCurrentLanguage(TimeMachineCard);
 TimeMachine.canHandle = (card) => card.type === TIME_MACHINE;
 
 export default TimeMachine;
