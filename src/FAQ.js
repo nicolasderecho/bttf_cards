8d9f7916b61157a1b0ab51ab21018bec841da7c1
Nicolas Derecho <nicolas.derecho@gmail.com>
2019-04-21 01:11:38 -0300

    Add FAQ component

---
+++ src/FAQ.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {EnglishFAQ} from "./EnglishFAQ";
+import './FAQ.scss'
+import logo from './assets/cardgamelogo.png';
+
+const FAQ = () => {
+    return (
+        <div className={'container'}>
+            <div className={'faq-header columns'}>
+                <div className={'column is-one-third'}>
+                    <figure className="image is-128x128"><img src={logo} alt={'logo'} className={'faq-logo'}/></figure>
+                </div>
+                <h2 className={'title is-2 column faq-title'}>FAQ</h2>
+            </div>
+            <div className={'faq'}>
+                {EnglishFAQ.map( (element) => <React.Fragment>
+                    <div className={'question'}>{element.question}</div>
+                    <div className={'answer'}>{element.answer}</div>
+                </React.Fragment>)}
+            </div>
+        </div>
+    );
+};
+
+export default FAQ;
